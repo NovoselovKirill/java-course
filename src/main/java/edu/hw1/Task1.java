be53a3fb8a379e40e850781e96d8b813cc0c3942
Kirill Novoselov <123.kirill.novoselov@gmail.com>
2023-10-10 02:08:02 +0500

    Solve Task1

---
+++ src/main/java/edu/hw1/Task1.java
@@ -0,0 +1,45 @@
+package edu.hw1;
+
+import java.util.Objects;
+
+public class Task1 {
+    private Task1() {
+    }
+
+    private static final String SEPARATOR = ":";
+    private static final int RESULT_ON_INCORRECT_INPUT = -1;
+    private static final int SECONDS_IN_MINUTE = 60;
+    private static final int MINUTES_IN_HOUR = 60;
+
+    private static Integer tryParse(String string) {
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public static int minutesToSeconds(String minutesAndSeconds) throws NullPointerException {
+        Objects.requireNonNull(minutesAndSeconds);
+
+        var parts = minutesAndSeconds.split(SEPARATOR);
+
+        if (parts.length != 2) {
+            return RESULT_ON_INCORRECT_INPUT;
+        }
+
+        Integer minutes = tryParse(parts[0]);
+        Integer seconds = tryParse(parts[1]);
+
+        if (minutes == null || seconds == null) {
+            return RESULT_ON_INCORRECT_INPUT;
+        }
+
+        var isValidMinutes = 0 <= minutes;
+        var isValidSeconds = 0 <= seconds && seconds < SECONDS_IN_MINUTE;
+
+        return isValidMinutes && isValidSeconds
+            ? minutes * MINUTES_IN_HOUR + seconds
+            : RESULT_ON_INCORRECT_INPUT;
+    }
+}
