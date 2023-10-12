c104407375f0103b54488c5a43ac850fa6e8cd22
Kirill Novoselov <123.kirill.novoselov@gmail.com>
2023-10-13 01:32:40 +0500

    Fix imports

--- src/test/java/edu/hw1/Task5Test.java
+++ src/test/java/edu/hw1/Task5Test.java
@@ -1,6 +1,6 @@
 package edu.hw1;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
